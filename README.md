76846862917ebfcb17ef3f18d0e0398e4c9817c3
S. Alex Stork <s.alex.stork@gmail.com>
2019-03-14 07:16:16 -0500

    Additional Features Added
    Features Added:
    - Added a Lives prompt so you can visually see how many lives you have left as you play
    - Provided display for what round user is on
    - Added a Game Over feature when the player runs out of lives
    - Added a ReadME explaining the rules, requirements, and objective of the game

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Classic-Arcade-Game-Clone
+THis Classic Arcade Game Clone is a spin-off of Frogger I created to practice my object oriented skills in JavaScript for Udacity's Front End Web Development NanoDegree. 
+
+## Requirements & Dependencies to Play
+In order to play this game locally you must download the entire ZIP file and extract it to your computer. There are images that are a part of the package that are required to render the objects in the game, including the environemnt as well as the player/bugs. 
+
+The HTML, CSS, and 3 JavaScript files are required as well so that the program will run as intended.
+
+You are also more than welcome to use the online version I created, which will also run the same as it does locally provided you have a solid Internet connection: https://alex-stork.github.io/Classic-Arcade-Game-Clone/
+
+## Objective & Rules
+The rules of the game are simple: get the player to the other side without running into any of the bugs. Once you cross the other side, you will be prompted to continue to the next round where you can do it again.
+
+The catch is you only get 5 lives for the entire game, and for every round you complete the bugs will get faster and faster. Your objective is to get the highest round possible without running out of lives entirely.
+
+## Contributing
+Although this game is for educational purposes only, pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 (function() {
+    let lives = 5;
+    let round = 1;
+    let multiplier = 50;
+    let roundComplete = false;
+    let gameOver = false;
 
-// Enemies our player must avoid
+    // Enemies our player must avoid
     var Enemy = function Enemy(x, y, speed) {
         // Variables applied to each of our instances go here,
         // we've provided one for you to get started
@@ -22,15 +27,30 @@
         if (this.x > 505) {
             this.x = -100;
             let newSpeed = Math.floor(Math.random() * 4 + 1);
-            this.speed = newSpeed * 60;
+            this.speed = newSpeed * multiplier;
         }
 
-        let enemyXleftMax = this.x - 70;
-        let enemyXRightMax = this.x + 70;
-        let enemyYTopMax = this.y - 60;
-        let enemyYBottomMax = this.y + 60;
+        let enemyXleftMax = this.x - 50;
+        let enemyXRightMax = this.x + 50;
+        let enemyYTopMax = this.y - 50;
+        let enemyYBottomMax = this.y + 50;
+        //When a user collides with the bug, player loses a life
         if (player.x > enemyXleftMax && player.x < enemyXRightMax && player.y > enemyYTopMax && player.y < enemyYBottomMax) {
-            player.resetPosition();
+            lives--;
+
+            if(lives <= 0) {
+                setTimeout(function () {
+                    gameOver = true;
+                    roundComplete = true;
+                    document.getElementById("endGame").style.display = 'block';
+                    document.getElementById("statsEndGame").innerHTML = `<h2>You made it to Round ${round} and ran out of lives. Try again!</h2>`
+                }, 100)
+            } 
+            
+            else {
+                document.querySelector("#lives").innerHTML = `Lives Left: ${lives}`
+                player.resetPosition();
+            }
         }
     };
     
@@ -51,6 +71,8 @@
     Player.prototype.resetPosition = function() {
         this.x = 202;
         this.y = 404;
+        lives = 5;
+        round = 1;
     };
 
     Player.prototype.render = function() {
@@ -59,47 +81,91 @@
 
     Player.prototype.handleInput = function(direction) {
         if (direction === 'left') { //Prevents player from going off canvas on left side
-            if (this.x >= 50) {
+            if (this.x > 50) {
                 this.x -= this.horizontal_move;
             } 
         }
+
         if (direction === 'right') {
-            if (this.x <= 400) { //Prevents player from going off canvas on right side
+            if (this.x < 400) { //Prevents player from going off canvas on right side
                 this.x += this.horizontal_move;
             } 
         }
+
         if (direction === 'up') {
-            if (this.y < 100) {
-                player.resetPosition();
-            }
+            if (this.y > 50) {
+
             this.y -= this.vertical_move;
+            }
+
+            if (this.y < 50) {
+                setTimeout(function () {
+                    roundComplete = true;
+                    if(endRound == true && endGame == false) {
+                        document.getElementById("endRound").style.display = 'block';
+                    }
+                    document.getElementById("stats").innerHTML = `<h2>You beat Round ${round} and have ${lives} lives left.</h2>`
+                }, 100)
+            }
         }
+
         if (direction === 'down') {
-            this.y += this.vertical_move;
+            if (this.y < 400) {
+                this.y += this.vertical_move;
+            }
         }
     };
 
     // Now instantiate your objects.
     window.player = new Player();
-    let bug1 = new Enemy(-80, 60 + 80 * 0, (Math.floor(Math.random() * 4 + 1) * 60));
-    let bug2 = new Enemy(-80, 60 + 80 * 1, (Math.floor(Math.random() * 4 + 1) * 60));
-    let bug3 = new Enemy(-80, 60 + 80 * 2, (Math.floor(Math.random() * 4 + 1) * 60));
+    let bug1 = new Enemy(-100, 60, (Math.floor(Math.random() * 4 + 1) * 60));
+    let bug2 = new Enemy(-100, 140, (Math.floor(Math.random() * 4 + 1) * 60));
+    let bug3 = new Enemy(-100, 220, (Math.floor(Math.random() * 4 + 1) * 60));
 
     // Place all enemy objects in an array called allEnemies
     window.allEnemies = [bug1, bug2, bug3];
 
+        Player.prototype.resetPosition = function () {
+            this.x = 202;
+            this.y = 404;
+        };
 
     // This listens for key presses and sends the keys to your
     // Player.handleInput() method. You don't need to modify this.
     document.addEventListener('keyup', function(e) {
-        let allowedKeys = {
-            37: 'left',
-            38: 'up',
-            39: 'right',
-            40: 'down',
-        };
+        if(roundComplete == false && gameOver == false) {
+            let allowedKeys = {
+                37: 'left',
+                38: 'up',
+                39: 'right',
+                40: 'down',
+            };
 
-        player.handleInput(allowedKeys[e.keyCode]);
+            player.handleInput(allowedKeys[e.keyCode]);
+        }
     });
 
+    //If there are no lives remaining, Enter will reset game, otherwise Enter will go to next round
+        addEventListener('keyup', function (e) {
+                if (e.keyCode === 13) {
+                    if (roundComplete == true && gameOver == false) {
+                        document.querySelector("#endRound").style.display = "none";
+                        roundComplete = false;
+                        player.resetPosition();
+                        multiplier += 10;
+                        round += 1;
+                        document.getElementById("round").innerHTML = `Round ${round}`;
+                }
+                    else if (gameOver == true) {
+                        document.querySelector("#endGame").style.display = "none";
+                        player.resetPosition();
+                        roundComplete = false;
+                        gameOver = false;
+                        lives = 5;
+                        round = 1;
+                        multiplier = 50;
+                        document.getElementById("round").innerHTML = `Round ${round}`;
+                    }
+            }
+    });
 })()
